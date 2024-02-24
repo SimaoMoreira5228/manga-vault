fd87b5d5ef74c3a3b1d2228d73fb79cac552f5db
Simão Moreira <apple.uncanny568@gmail.com>
2024-02-24 01:31:14 +0000

    Refactor on upload and implemented fetch_external_image

--- api/src/files.rs
+++ api/src/files.rs
@@ -1,9 +1,8 @@
 use actix_multipart::Multipart;
 use actix_web::{get, post, web, HttpResponse, Responder};
 use database::Database;
-use futures_util::StreamExt;
+use futures_util::{StreamExt, TryStreamExt};
 use std::{
-	ffi::OsStr,
 	io::Write,
 	path::Path,
 	sync::{Arc, Mutex},
@@ -20,56 +19,35 @@ async fn upload_file(db: web::Data<Arc<Mutex<Database>>>, mut payload: Multipart
 		std::fs::create_dir_all(&path).expect("Failed to create uploads directory");
 	}
 
-	while let Some(Ok(mut field)) = payload.next().await {
-		let content_disposition = field.content_disposition().clone();
+	let mut data = Vec::new();
 
-		let old_name = content_disposition.get_filename().unwrap();
-		let id = uuid::Uuid::new_v4().to_string();
-
-		let file_extension = Path::new(&old_name).extension().and_then(OsStr::to_str);
-
-		if let Some(extension) = file_extension {
-			if !["jpg", "jpeg", "png", "gif"].contains(&extension) {
-				return HttpResponse::BadRequest().body("Unsupported file type");
-			}
-		} else {
-			return HttpResponse::BadRequest().body("No file extension found");
-		}
-
-		let filepath = format!("{}/{}", path.display(), id);
-		println!("filepath: {}", filepath);
-		let mut file = match std::fs::File::create(filepath) {
-			Ok(file) => file,
-			Err(e) => return HttpResponse::InternalServerError().body(format!("Error: {}", e)),
-		};
-
+	while let Some(item) = payload.next().await {
+		let mut field = item.unwrap().inspect_err(|e| println!("Error: {}", e)).map_err(|e| {
+			println!("Error: {}", e);
+			HttpResponse::InternalServerError().body("Error")
+		});
 		while let Some(chunk) = field.next().await {
-			let data = match chunk {
-				Ok(data) => data,
-				Err(e) => return HttpResponse::InternalServerError().body(format!("Error: {}", e)),
-			};
-
-			file = match file.write_all(&data) {
-				Ok(_) => file,
-				Err(e) => return HttpResponse::InternalServerError().body(format!("Error: {}", e)),
-			};
+			data.extend_from_slice(&chunk.unwrap());
 		}
-
-		let db = db.lock().unwrap();
-		db.insert_file(&id, &old_name).unwrap();
-
-		file = match file.sync_all() {
-			Ok(_) => file,
-			Err(e) => return HttpResponse::InternalServerError().body(format!("Error: {}", e)),
-		};
-
-		match file.flush() {
-			Ok(_) => file,
-			Err(e) => return HttpResponse::InternalServerError().body(format!("Error: {}", e)),
-		};
 	}
 
-	HttpResponse::Ok().body("File uploaded")
+	if !infer::is_image(&data) {
+		return HttpResponse::BadRequest().body("Invalid image");
+	}
+
+	let unique_signature = uuid::Uuid::new_v4().to_string();
+	let kind = infer::get(&data).unwrap();
+	let image_url = format!("{}.{}", unique_signature, kind.extension());
+
+	let file_path = format!("{}/{}", path.display(), image_url);
+	let mut file = std::fs::File::create(&file_path).expect("Failed to create file");
+
+	file.write_all(&data).expect("Failed to write to file");
+
+	let db = db.lock().unwrap();
+	db.insert_file(&unique_signature, &image_url).unwrap();
+
+	HttpResponse::Ok().body(format!("File uploaded: {}", image_url))
 }
 
 #[get("/image/{id}")]
@@ -80,22 +58,15 @@ async fn get_image(db: web::Data<Arc<Mutex<Database>>>, id: web::Path<String>) -
 	let path = format!("{}/uploads", CONFIG.directory);
 	let path = Path::new(&path);
 
-	let file = match std::fs::read(format!("{}/{}", path.display(), file.id)) {
+	let file = match std::fs::read(format!("{}/{}", path.display(), file.name)) {
 		Ok(file) => file,
 		Err(e) => return HttpResponse::InternalServerError().body(format!("Error: {}", e)),
 	};
 
-	let file_extension = path.extension().and_then(OsStr::to_str);
+	let kind = infer::get(&file).unwrap();
+	let mime = kind.mime_type();
 
-	let content_type = match file_extension {
-		Some("jpg") => "image/jpeg",
-		Some("jpeg") => "image/jpeg",
-		Some("png") => "image/png",
-		Some("gif") => "image/gif",
-		_ => "application/octet-stream",
-	};
-
-	HttpResponse::Ok().content_type(content_type).body(file)
+	HttpResponse::Ok().content_type(mime).body(file)
 }
 
 #[get("download/{id}")]
@@ -106,15 +77,41 @@ async fn download_file(db: web::Data<Arc<Mutex<Database>>>, id: web::Path<String
 	let path = format!("{}/uploads", CONFIG.directory);
 	let path = Path::new(&path);
 
-	let file = match std::fs::read(format!("{}/{}", path.display(), db_file.id)) {
+	let file = match std::fs::read(format!("{}/{}", path.display(), db_file.name)) {
 		Ok(file) => file,
 		Err(e) => return HttpResponse::InternalServerError().body(format!("Error: {}", e)),
 	};
 
-	let stream = futures_util::stream::once(async move { Ok::<_, std::io::Error>(actix_web::web::Bytes::from(file)) });
+	let kind = infer::get(&file).unwrap();
+	let mime = kind.mime_type();
 
 	HttpResponse::Ok()
-		.content_type("application/octet-stream")
-		.append_header(("Content-Disposition", format!("attachment; filename={}", db_file.name)))
-		.streaming(stream)
+		.content_type(mime)
+		.insert_header(("Content-Disposition", format!("attachment; filename={}", db_file.name)))
+		.body(file)
+}
+
+pub async fn fetch_external_image(url: &str) -> HttpResponse {
+	let res = reqwest::get(url).await;
+	if res.is_err() {
+		println!("Failed to use url: {}", url);
+		HttpResponse::BadRequest().body("The specified website failed to respond.");
+	}
+
+	let res = res.unwrap();
+
+	if !res.status().is_success() {
+		HttpResponse::BadRequest().body("The specified website failed to respond.");
+	}
+
+	let data = res.bytes().await.unwrap().to_vec();
+
+	if !infer::is_image(&data) {
+		return HttpResponse::BadRequest().body("Invalid image");
+	}
+
+	let kind = infer::get(&data).unwrap();
+	let mime = kind.mime_type();
+
+	HttpResponse::Ok().content_type(mime).body(data)
 }
