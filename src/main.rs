46eabb50045aed5ef2aad593f24f408adbc2fcee
Simão Moreira <apple.uncanny568@gmail.com>
2024-03-29 20:48:18 +0000

    Update panic message to include link to latest version

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ async fn main() {
 	let latest_version = api::downloader::get_version("SimaoMoreira5228", "manga-vault").await.unwrap();
 
 	if latest_version != VERSION {
-		panic!("Please update the application to the latest version");
+		panic!(
+			"Please update the application to the latest version https://github.com/SimaoMoreira5228/manga-vault/releases/latest"
+		);
 	} else {
 		println!("Application is up to date");
 	}
