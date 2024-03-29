9fcdc95c8a36cf54d4ecedd173aa5e09e145ca6e
Simão Moreira <apple.uncanny568@gmail.com>
2024-03-29 16:18:27 +0000

    Add mark_all_as_read and mark_all_as_unread routes

--- api/src/routes/read_chapter.rs
+++ api/src/routes/read_chapter.rs
@@ -16,6 +16,18 @@ struct MarkAsUnread {
 	pub chapter_id: i32,
 }
 
+#[derive(serde::Deserialize)]
+struct MarkAllAsread {
+	pub user_id: i32,
+	pub manga_id: i32,
+}
+
+#[derive(serde::Deserialize)]
+struct MarkAllAsUnread {
+	pub user_id: i32,
+	pub manga_id: i32,
+}
+
 #[post("user/{user_id}/read/{chapter_id}/mark-as-read")]
 async fn mark_as_read(db: web::Data<connection::Connection>, params: web::Json<MarkAsRead>) -> impl Responder {
 	let user: Option<crate::entities::users::Model> = crate::entities::users::Entity::find_by_id(params.user_id)
@@ -73,6 +85,60 @@ async fn mark_as_read(db: web::Data<connection::Connection>, params: web::Json<M
 	HttpResponse::Ok().body("Chapter marked as read")
 }
 
+#[post("user/{user_id}/read/{manga_id}/mark-all-as-read")]
+async fn mark_all_as_read(db: web::Data<connection::Connection>, params: web::Json<MarkAllAsread>) -> impl Responder {
+	let user: Option<crate::entities::users::Model> = crate::entities::users::Entity::find_by_id(params.user_id)
+		.one(db.get_ref())
+		.await
+		.unwrap();
+
+	if user.is_none() {
+		return HttpResponse::BadRequest().body("User not found");
+	}
+
+	let user = user.unwrap();
+
+	let manga: Option<crate::entities::mangas::Model> = crate::entities::mangas::Entity::find_by_id(params.manga_id)
+		.one(db.get_ref())
+		.await
+		.unwrap();
+
+	if manga.is_none() {
+		return HttpResponse::BadRequest().body("Manga not found");
+	}
+
+	let manga = manga.unwrap();
+
+	let chapters = Chapters::find()
+		.filter(crate::entities::chapters::Column::MangaId.eq(manga.id))
+		.all(db.get_ref())
+		.await
+		.unwrap();
+
+	for chapter in chapters {
+		let read_chapter: Option<crate::entities::read_chapters::Model> = ReadChapters::find()
+			.filter(crate::entities::read_chapters::Column::UserId.eq(user.id))
+			.filter(crate::entities::read_chapters::Column::ChapterId.eq(chapter.id))
+			.one(db.get_ref())
+			.await
+			.unwrap();
+
+		if read_chapter.is_none() {
+			let _ = crate::entities::read_chapters::ActiveModel {
+				user_id: Set(user.id),
+				chapter_id: Set(chapter.id),
+				manga_id: Set(manga.id),
+				created_at: Set(chrono::Utc::now().naive_utc().to_string()),
+				..Default::default()
+			}
+			.insert(db.get_ref())
+			.await;
+		}
+	}
+
+	HttpResponse::Ok().body("All chapters marked as read")
+}
+
 #[post("user/{user_id}/read/{chapter_id}/mark-as-unread")]
 async fn mark_as_unread(db: web::Data<connection::Connection>, params: web::Json<MarkAsUnread>) -> impl Responder {
 	let user: Option<crate::entities::users::Model> = crate::entities::users::Entity::find_by_id(params.user_id)
@@ -116,6 +182,51 @@ async fn mark_as_unread(db: web::Data<connection::Connection>, params: web::Json
 	}
 
 	HttpResponse::Ok().body("Chapter marked as unread")
+}
+
+#[post("user/{user_id}/read/{manga_id}/mark-all-as-unread")]
+async fn mark_all_as_unread(db: web::Data<connection::Connection>, params: web::Json<MarkAllAsUnread>) -> impl Responder {
+	let user: Option<crate::entities::users::Model> = crate::entities::users::Entity::find_by_id(params.user_id)
+		.one(db.get_ref())
+		.await
+		.unwrap();
+
+	if user.is_none() {
+		return HttpResponse::BadRequest().body("User not found");
+	}
+
+	let user = user.unwrap();
+
+	let manga: Option<crate::entities::mangas::Model> = crate::entities::mangas::Entity::find_by_id(params.manga_id)
+		.one(db.get_ref())
+		.await
+		.unwrap();
+
+	if manga.is_none() {
+		return HttpResponse::BadRequest().body("Manga not found");
+	}
+
+	let manga = manga.unwrap();
+
+	let read_chapters = ReadChapters::find()
+		.filter(crate::entities::read_chapters::Column::UserId.eq(user.id))
+		.filter(crate::entities::read_chapters::Column::MangaId.eq(manga.id))
+		.all(db.get_ref())
+		.await
+		.unwrap();
+
+	for read_chapter in read_chapters {
+		let res: DeleteResult = read_chapter
+			.delete(db.get_ref())
+			.await
+			.expect("Failed to delete read chapter");
+
+		if res.rows_affected == 0 {
+			return HttpResponse::InternalServerError().body("Failed to delete read chapter");
+		}
+	}
+
+	HttpResponse::Ok().body("All chapters marked as unread")
 }
 
 #[post("user/{user_id}/read/{chapter_id}/is-read")]
@@ -196,7 +307,9 @@ async fn get_read_chapters(db: web::Data<connection::Connection>, params: web::P
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
 	cfg.service(mark_as_read);
+	cfg.service(mark_all_as_read);
 	cfg.service(mark_as_unread);
+	cfg.service(mark_all_as_unread);
 	cfg.service(is_read);
 	cfg.service(get_read_chapters);
 }
