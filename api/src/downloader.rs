0d5fd1e4277c87c4f5534ce9ec77e410ba3cc5fb
Simão Moreira <apple.uncanny568@gmail.com>
2024-10-31 17:21:10 +0000

    Implement website update functionality and refactor version checking logic

--- api/src/downloader.rs
+++ api/src/downloader.rs
@@ -1,7 +1,12 @@
-use std::fs;
+use std::{
+	fs::{self, DirEntry},
+	io::BufRead,
+};
 
 use reqwest::{header, Client};
 use serde_json::Value;
+
+use crate::CONFIG;
 
 pub async fn downloader(path: &str, owner: &str, repo: &str) -> Result<(), Box<dyn std::error::Error>> {
 	let release_url = format!("https://api.github.com/repos/{}/{}/releases/latest", owner, repo);
@@ -66,4 +71,54 @@ pub async fn unzip_file(path: &str, dir: &str) -> Result<(), Box<dyn std::error:
 	fs::remove_file(path).unwrap();
 
 	Ok(())
+}
+
+pub async fn update_website() {
+	let website_dir = format!("{}/website", CONFIG.directory);
+	if !std::path::Path::new(&website_dir).exists() {
+		std::fs::create_dir(&website_dir).unwrap();
+	}
+
+	let website_path = std::path::Path::new(&website_dir);
+	let entries = fs::read_dir(&website_path).unwrap();
+	let mut website_version_file: Option<DirEntry> = None;
+	let mut website_build_folder: Option<DirEntry> = None;
+	for entry in entries {
+		let file = entry.unwrap();
+		let file_name = file.file_name();
+		let file_name = file_name.to_str().unwrap();
+		match file_name {
+			"version.txt" => website_version_file = Some(file),
+			"build" => website_build_folder = Some(file),
+			_ => (),
+		}
+	}
+	let latest_version = get_version("SimaoMoreira5228", "manga-vault").await.unwrap();
+
+	if !website_version_file.is_some() {
+		if website_build_folder.is_some() {
+			fs::remove_dir_all(format!("{}/build", website_dir)).unwrap();
+		}
+
+		downloader(&website_dir, "SimaoMoreira5228", "manga-vault").await.unwrap();
+		unzip_file(format!("{}/website.zip", website_dir).as_str(), &CONFIG.directory)
+			.await
+			.unwrap();
+		fs::write(format!("{}/version.txt", website_dir), latest_version).unwrap();
+	} else if website_version_file.is_some() {
+		let file = fs::File::open(format!("{}/version.txt", website_dir)).unwrap();
+		let version = std::io::BufReader::new(file).lines().next().unwrap().unwrap();
+
+		if version != latest_version {
+			if website_build_folder.is_some() {
+				fs::remove_dir_all(format!("{}/build", website_dir)).unwrap();
+			}
+
+			downloader(&website_dir, "SimaoMoreira5228", "manga-vault").await.unwrap();
+			unzip_file(format!("{}/website.zip", website_dir).as_str(), &CONFIG.directory)
+				.await
+				.unwrap();
+			fs::write(format!("{}/version.txt", website_dir), latest_version).unwrap();
+		}
+	}
 }
